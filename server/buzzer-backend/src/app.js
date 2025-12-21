8c76996e7bfa2c28420b9b798367a03c6b5a5302
Marwan <marwanabdalrady13@gmail.com>
2025-12-21 13:32:23 +0200

    Update app.js

--- server/buzzer-backend/src/app.js
+++ server/buzzer-backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://buzzer-restaurant-hyqkxh78h-marwans-projects-3f5c5d0c.vercel.app/'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
